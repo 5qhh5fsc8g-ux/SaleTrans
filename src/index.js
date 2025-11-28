d408adec4675842fa74f01a5603e94e702c87177
5qhh5fsc8g-ux <5qhh5fsc8g@privaterelay.appleid.com>
2025-11-28 16:10:56 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './SmartSaver';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
